d7a6f8a13144e339c6de60832faa8a89304b4aef
dabdevelop <develop@dab.vc>
2017-08-14 15:13:07 +0800

    Add documentations.

--- solidity/contracts/VoteTokenController.sol
+++ solidity/contracts/VoteTokenController.sol
@@ -3,7 +3,18 @@ pragma solidity ^0.4.11;
 import './interfaces/ISmartToken.sol';
 import './SmartTokenController.sol';
 
+
+/*
+    Vote Token Controller
+
+*/
 contract VoteTokenController is SmartTokenController{
+
+/**
+    @dev constructor
+
+    @param _token       vote token
+*/
     function VoteTokenController(ISmartToken _token)
     SmartTokenController(_token) {}
 }
